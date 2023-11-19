6634ce057539be44c0ecc5da4f540c8e5828369b
hetncr <97675352+hetncr@users.noreply.github.com>
2023-11-19 14:11:20 +0300

    Update sektorler_1.py

--- sektorler_1.py
+++ sektorler_1.py
@@ -18,46 +18,46 @@ Original file is located at
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile deneme.py
 # # Import the necessary modules
-# import requests
-# from bs4 import BeautifulSoup
-# import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import streamlit as st
 # 
 # # Web scraping code to get the sector name
-# url = "https://www.isyatirim.com.tr/tr-tr/analiz/hisse/Sayfalar/Temel-Degerler-Ve-Oranlar.aspx?"
-# response = requests.get(url)
-# temeldegerler = BeautifulSoup(response.text, "html.parser")
+url = "https://www.isyatirim.com.tr/tr-tr/analiz/hisse/Sayfalar/Temel-Degerler-Ve-Oranlar.aspx?"
+response = requests.get(url)
+temeldegerler = BeautifulSoup(response.text, "html.parser")
 # 
 # # Find the table with the desired data
-# table = temeldegerler.find("tbody", id="temelTBody_Ozet")
+table = temeldegerler.find("tbody", id="temelTBody_Ozet")
 # 
 # # Create an empty dictionary to store the stock name and sector name pairs
-# hisse_sektor = {}
+hisse_sektor = {}
 # 
 # # Iterate over the rows in the table
-# for row in table.find_all("tr"):
+for row in table.find_all("tr"):
 #     # Get the cells in the row
-#     cells = row.find_all("td")
+    cells = row.find_all("td")
 #     # Get the stock name
-#     hisse = cells[0].find("a").text
+     hisse = cells[0].find("a").text
 #     # Get the sector name
-#     sektor = cells[2].text
+     sektor = cells[2].text
 #     # Add the pair to the dictionary
-#     hisse_sektor[hisse] = sektor
+     hisse_sektor[hisse] = sektor
 # 
 # # Create an input box to enter the stock name
-# hisse_input = st.text_input("Hisse Adı:")
+hisse_input = st.text_input("Hisse Adı:")
 # 
 # # Check if the input is not empty
-# if hisse_input:
+if hisse_input:
 #     # Check if the input is in the dictionary
-#     if hisse_input in hisse_sektor:
+     if hisse_input in hisse_sektor:
 #         # Get the sector name from the dictionary
-#         sektor_output = hisse_sektor[hisse_input]
+         sektor_output = hisse_sektor[hisse_input]
 #         # Display the sector name
-#         st.write("Sektör Alanı:", sektor_output)
+         st.write("Sektör Alanı:", sektor_output)
 #     else:
 #         # Display an error message
-#         st.write("Hisse bulunamadı.")
+         st.write("Hisse bulunamadı.")
 #
 
-!streamlit run deneme.py & npx localtunnel --port 8501
+#!streamlit run deneme.py & npx localtunnel --port 8501
